8a9eb7e23523a85a28267b7139e0bc98d06e1fe3
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-13 14:35:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+const First= function  returnFirstTwoDrivers(drivers)
+{
+let Firstvalue=drivers[0]
+let secondtvalue=drivers[1]
+console.log(Firstvalue, secondtvalue);
+}
+First(drivers)
